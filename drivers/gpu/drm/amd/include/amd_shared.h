51e59b0228456429bdc8ded33c8285c26e070cce
Yohello1 <siri@racha.ca>
2026-06-01 16:17:56 -0400

    drm/amdgpu: update hte handle ptr in sw_fini

--- drivers/gpu/drm/amd/include/amd_shared.h
+++ drivers/gpu/drm/amd/include/amd_shared.h
@@ -384,7 +384,7 @@ struct amd_ip_funcs {
 	int (*early_init)(struct amdgpu_ip_block *ip_block);
 	int (*late_init)(struct amdgpu_ip_block *ip_block);
 	int (*sw_init)(struct amdgpu_ip_block *ip_block);
-	int (*sw_fini)(void *handle);
+	int (*sw_fini)(struct amdgpu_ip_block *ip_block);
 	int (*early_fini)(struct amdgpu_ip_block *ip_block);
 	int (*hw_init)(void *handle);
 	int (*hw_fini)(void *handle);
