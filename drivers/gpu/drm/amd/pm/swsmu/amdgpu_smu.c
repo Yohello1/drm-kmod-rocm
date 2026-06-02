d47692540e8d78eb17e052f49004d7f29bda7de7
Yohello1 <siri@racha.ca>
2026-06-02 14:08:50 -0400

    Merged via lenient fallback: 1083-drm-amdgpu-pass-ip_block-in-set_powergating_state.patch

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -2210,7 +2210,7 @@ static int smu_set_clockgating_state(void *handle,
 	return 0;
 }
 
-static int smu_set_powergating_state(void *handle,
+static int smu_set_powergating_state(struct amdgpu_ip_block *ip_block,
 				     enum amd_powergating_state state)
 {
 	return 0;
